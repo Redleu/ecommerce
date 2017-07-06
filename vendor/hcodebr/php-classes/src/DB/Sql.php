b9f5753bde09682ec01b37b6412b0b5c47ad99f3
Uelder Lima <uelder_lima@hotmail.com>
2017-07-05 22:35:15 -0300

    ok

--- vendor/hcodebr/php-classes/src/DB/Sql.php
+++ vendor/hcodebr/php-classes/src/DB/Sql.php
@@ -6,7 +6,7 @@ class Sql {
 
 	const HOSTNAME = "127.0.0.1";
 	const USERNAME = "root";
-	const PASSWORD = "uol318dev";
+	const PASSWORD = "3##########";
 	const DBNAME = "db_ecommerce";
 
 	private $conn;
